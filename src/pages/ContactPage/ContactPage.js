9f94e0e278163b1a9277726ed238eb8315c5f635
Alain GUILLON <zyrass@outlook.fr>
2022-11-01 21:01:57 +0100

    update content ContactPage

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,5 +1,33 @@
 function ContactPage() {
-  return <h1>Contact</h1>;
+  return (
+    <div>
+      <h1>Contactez-nous</h1>
+      <h2>Vous avez une question ou une remarque ?</h2>
+      <section>
+        <div>
+          <span>Nom</span>
+          <input type="text" placeholder="Veuillez saisir votre nom ici..." />
+        </div>
+        <div>
+          <span>Email</span>
+          <input
+            type="email"
+            placeholder="Veuillez saisir votre e-mail ici..."
+          />
+        </div>
+        <div>
+          <span>Votre message</span>
+          <textarea
+            type="textarea"
+            placeholder="Veuillez saisir votre message ici. Il peut s'agir d'une question ou bien d'une remarque."
+          />
+        </div>
+        <div>
+          <button type="button">Soumettre</button>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default ContactPage;
