38e2e4ce95493ca66e5bececa4b4fbefe9f23ba2
Alain GUILLON <zyrass@outlook.fr>
2022-11-02 22:41:32 +0100

    update responsive LocalisationPage

--- src/pages/LocalisationPage/LocalisationPage.js
+++ src/pages/LocalisationPage/LocalisationPage.js
@@ -3,41 +3,42 @@ import imgLocalisation from "../../assets/images/cafe2.png";
 
 function LocalisationPage() {
   return (
-    <div className={`container-fluid bg-white ${styles.localisation}`}>
-      <h1
-        className={`upper py-50 px-100 d-flex justify-content-center ${styles.h1}`}
-      >
-        Localisation
-      </h1>
-      <section className={`${styles.plan} py-0 px-25`}>
+    <div className={`bg-white ${styles.localisation}`}>
+      <h1 className={`upper ${styles.h1}`}>Localisation</h1>
+      <section className={`${styles.plan}`}>
         <img
           className={styles.img}
+          style={{
+            backgroundSize: "cover",
+          }}
           src={imgLocalisation}
           alt="localisation du restaurent"
         />
       </section>
-      <section className="py-0 adresse">
-        <h2 className={styles.h2}>Où nous trouver</h2>
-        <hr className="w-70 mx-auto my-30" />
 
-        <div className="alert">
-          <p className="d-flex justify-content-between w-100">
-            <strong className="w-30">
-              Métro ligne <mark>4</mark>
+      <section className="adresse">
+        <h2 className={styles.h2}>Où nous trouver</h2>
+        <hr className="w-60 mx-auto my-30" />
+
+        <div className="alert w-90 mx-auto mb-20">
+          <p className="d-flex align-items-center justify-content-between w-100">
+            <strong>
+            <mark>Métro ligne 4</mark>
             </strong>
             <span>Odéon</span>
           </p>
         </div>
-        <div className="alert mt-20 mb-70">
-          <p className="d-flex justify-content-between w-100">
-            <strong className="w-30">
-              Métro ligne <mark>11</mark>
+
+        <div className="alert w-90 mx-auto mb-20">
+          <p className="d-flex align-items-center justify-content-between w-100">
+            <strong>
+            <mark>Métro ligne 11</mark>
             </strong>
             <span>Mabillon</span>
           </p>
         </div>
 
-        <address className="d-flex justify-content-between mr-70">
+        <address className={styles.adresse}>
           <strong>Adresse:</strong>
           <mark>131 Boulevard Saint-Germain, 75006 Paris</mark>
         </address>
