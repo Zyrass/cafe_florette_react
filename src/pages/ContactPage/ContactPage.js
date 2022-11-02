9b39c760a70201449f6035815b03ac1a98b25073
Alain GUILLON <zyrass@outlook.fr>
2022-11-02 14:51:42 +0100

    updtate ContactPage

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,15 +1,17 @@
 import styles from "./Contact.module.scss";
 
 function ContactPage() {
+
   return (
-    <div className="container mt-40 px-40 pb-40 bg-white">
-      <h1 className={`upper pt-40 my-40 ${styles.h1}`}>Contactez-nous</h1>
+    <div className="container mt-30 px-40 pb-30 bg-white">
+      <h1 className={`upper pt-30 my-30 ${styles.h1}`}>Contactez-nous</h1>
       <h2>Vous avez une question ou une remarque ?</h2>
       <section className="py-40 px-40">
         <div className="d-flex flex-column mb-20">
           <span className="mb-5">Nom</span>
-          <input 
-            type="text" placeholder="Veuillez saisir votre nom ici..." 
+          <input
+            type="text"
+            placeholder="Veuillez saisir votre nom ici..."
             className="py-5 px-15"
           />
         </div>
@@ -30,8 +32,15 @@ function ContactPage() {
           />
         </div>
         <hr className="my-50 w-50 mx-auto" />
-        <div className="d-flex justify-content-center">
-          <button type="button" className="upper btn btn-primary">Soumettre</button>
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="upper btn btn-primary w-40 mr-25"
+            disabled
+          >
+            Soumettre
+          </button>
+          <em>Le formulaire ne fera rien.</em>
         </div>
       </section>
     </div>
