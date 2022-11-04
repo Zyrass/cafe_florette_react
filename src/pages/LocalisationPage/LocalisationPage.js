65ec5fda5cefba922495611b4cf1f7492d49d96f
Alain GUILLON <zyrass@outlook.fr>
2022-11-04 14:59:28 +0100

    update responsive localisation all size

--- src/pages/LocalisationPage/LocalisationPage.js
+++ src/pages/LocalisationPage/LocalisationPage.js
@@ -5,45 +5,40 @@ import Alert from "./Alert/Alert";
 import styles from "./LocalisationPage.module.scss";
 
 // image café-2
-import imgLocalisation from "../../assets/images/cafe2.png";
+import imgPlan from "../../assets/images/cafe2.png";
 
 function LocalisationPage() {
   return (
     <div className={styles.localisation}>
       <h1>Localisation</h1>
-      <section className={styles.plan}>
-        <img
-          className={styles.img}
-          style={{
-            backgroundSize: "cover",
-          }}
-          src={imgLocalisation}
-          alt="localisation du restaurent"
-        />
-      </section>
+      <div>
+        <section>
+          <img src={imgPlan} alt="localisation du restaurent" />
+        </section>
 
-      <section>
-        <h2 className={styles.h2}>Où nous trouver</h2>
-        <hr className="w-60 mx-auto my-30" />
+        <section>
+          <h2>Où nous trouver</h2>
+          <hr className="w-60 mx-auto my-30" />
 
-        <Alert
-          key="3145321-1564832183"
-          typeAlert="normal"
-          ligne={4}
-          name="Odéon"
-        />
-        <Alert
-          key="4565891-1235981255"
-          typeAlert="normal"
-          ligne={11}
-          name="Mabillon"
-        />
+          <Alert
+            key="3145321-1564832183"
+            typeAlert="normal"
+            ligne={4}
+            name="Odéon"
+          />
+          <Alert
+            key="4565891-1235981255"
+            typeAlert="normal"
+            ligne={11}
+            name="Mabillon"
+          />
 
-        <address className={styles.adresse}>
-          <strong>Adresse:</strong>
-          <mark>131 Boulevard Saint-Germain, 75006 Paris</mark>
-        </address>
-      </section>
+          <address>
+            <strong>Adresse:</strong>
+            <mark>131 Boulevard Saint-Germain, 75006 Paris</mark>
+          </address>
+        </section>
+      </div>
     </div>
   );
 }
